67b5d63a5020081972d8358e964d600f58585bf0
jisanghoon <246060@naver.com>
2016-12-13 14:51:03 +0900

    helloworld.java 한 줄 추가

--- src/kr/or/digit/bigdata/helloworld/HelloWorld.java
+++ src/kr/or/digit/bigdata/helloworld/HelloWorld.java
@@ -3,7 +3,8 @@ package kr.or.digit.bigdata.helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		
+		System.out.println("Hello World");
+		System.out.println("hotfix에서 작업");
 	}
 
 }
